adb88fed3210572eb77cc0a61f80bcabf4abe699
Humam Ibadillah <humamifakhri@gmail.com>
2022-08-17 17:50:00 +0700

    [ALL] Navbar and Footer

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let activePage = window.location.pathname;
+let navLinks = document.querySelectorAll('.nav-link').
+forEach(link => {
+  if(link.href.includes(`${activePage}`)){
+    link.classList.add('active');
+  }
+});
