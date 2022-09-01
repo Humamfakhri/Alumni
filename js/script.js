6cdf080b784eba9dff10c5acd0da258431d38f10
Humam Ibadillah <humamifakhri@gmail.com>
2022-09-01 10:03:53 +0700

    Membuat Carousel Landing Page

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-let activePage = window.location.pathname;
-let navLinks = document.querySelectorAll('.nav-link').
-forEach(link => {
-  if(link.href.includes(`${activePage}`)){
-    link.classList.add('active');
-  }
-});
+// let activePage = window.location.pathname;
+// let navLinks = document.querySelectorAll('.nav-link').
+// forEach(link => {
+//   if(link.href.includes(`${activePage}`)){
+//     link.classList.add('active');
+//   }
+// });
 
-like = (icon) => icon.classList.toggle('fa-solid');
+// like = (icon) => icon.classList.toggle('fa-solid');
